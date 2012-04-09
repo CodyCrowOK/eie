1885ffd081c554bbaed4e30b6b9b47ffb479c669
Cody Crow <cody@cms07.org>
2012-04-08 21:45:50 -0500

    given/when

--- eie.pl
+++ eie.pl
@@ -2,12 +2,32 @@
 
 use strict;
 use warnings;
+use feature ":5.10";
+use File::Path qw(make_path remove_tree);
 
-switch ($ARGV[0]) {
-	case "init":
-		break;
-	case "add":
-		break;
-	case "commit":
+our $eie_dir = ".eie";
+
+sub eie_init() {
+	mkdir $eie_dir or die("Could not create eie directory.\n");
+}
+
+sub eie_clean() {
+	remove_tree($eie_dir) or die("Could not rm eie directory.\n");
+}
+
+given($ARGV[0]) {
+	when ("init") {
+		eie_init();
 		break;
 	}
+	when ("add") {
+		break;
+	}
+	when ("commit") {
+		break;
+	}
+	when ("clean") {
+		eie_clean();
+		break;
+	}
+}
